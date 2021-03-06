861cf7075f1adfba1856cc3659253d7fa00a845b
Jan Bischoff <44364488+P4P3RW0RLD@users.noreply.github.com>
2021-03-06 01:26:00 +0100

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ btnPlus.onclick = function(){
 btnMinus.onclick = function(){
 }
 
-canvas.addEventListener('touchstart', handleTouchStart, false);
+/*canvas.addEventListener('touchstart', handleTouchStart, false);
 canvas.addEventListener('touchmove', handleTouchMovement, false);
 
 var xDown = null;
@@ -33,4 +33,4 @@ function handleTouchMovement(evt){
     var xUp = evt.touches[0].clientX;
     var xDiff = xDown - xUp;
 
-}
+}*/
